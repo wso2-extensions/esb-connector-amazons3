c87c4faeef697ec34fa78b42998a28c508e200bd
Shakila <srajah.shakila@gmail.com>
2016-06-11 20:52:37 +0530

    Removing unwanted header.

--- src/test/java/org/wso2/carbon/connector/integration/test/amazons3/AmazonS3ConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/amazons3/AmazonS3ConnectorIntegrationTest.java
@@ -2094,7 +2094,6 @@ public class AmazonS3ConnectorIntegrationTest extends ESBIntegrationTest {
         esbRequestHeadersMap.put("Action", "urn:uploadPart");
 
         esbRequestHeadersMap.put("Authorization", authorizationCode);
-        esbRequestHeadersMap.put("isXAmzDate", "true");
         esbRequestHeadersMap.put("x-amz-date", dateValue);
         esbRequestHeadersMap.put("Content-Type", "text/plain; charset=UTF-8");
 
