7df114b64042514038535386f33f38eae7a4031c
Hariprasath <tharis63@gmail.com>
2016-11-04 15:26:54 +0530

    changed the version

--- src/test/java/org/wso2/carbon/connector/integration/test/amazons3/AmazonS3ConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/amazons3/AmazonS3ConnectorIntegrationTest.java
@@ -55,7 +55,7 @@ public class AmazonS3ConnectorIntegrationTest extends ESBIntegrationTest {
 
     private String repoLocation = null;
 
-    private String amazons3ConnectorFileName = CONNECTOR_NAME + "-connector-1.0.3.zip";
+    private String amazons3ConnectorFileName = CONNECTOR_NAME + "-connector-1.0.4-SNAPSHOT.zip";
 
     private Properties amazons3ConnectorProperties = null;
 
